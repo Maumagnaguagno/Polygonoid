7d15fd38551029fb55b7828cd0f4dfbb8932adcf
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-11-03 21:39:41 -0200

    Move equality test to match order

--- tests/pointy.rb
+++ tests/pointy.rb
@@ -9,19 +9,19 @@ class Pointy < Test::Unit::TestCase
     assert_in_epsilon(50.0, point.y)
   end
 
+  def test_point_equality
+    assert_equal(true, Point.new(5,5) == Point.new(5.0,5.0))
+    assert_equal(true, Point.new(0.1 + 0.2,2) == Point.new(0.3,2.000000000000000000004))
+    assert_equal(false, Point.new(5,6) == Point.new(6,5))
+    assert_equal(false, Point.new(5.1,5) == Point.new(5,5))
+  end
+
   def test_point_distance
     origin = Point.new(0,0)
     point = Point.new(1,1)
     sqrt2 = Math.sqrt(2)
     assert_in_epsilon(sqrt2, origin.distance(point))
     assert_in_epsilon(sqrt2, point.distance(origin))
-  end
-
-  def test_point_equality
-    assert_equal(true, Point.new(5,5) == Point.new(5.0,5.0))
-    assert_equal(true, Point.new(0.1 + 0.2,2) == Point.new(0.3,2.000000000000000000004))
-    assert_equal(false, Point.new(5,6) == Point.new(6,5))
-    assert_equal(false, Point.new(5.1,5) == Point.new(5,5))
   end
 
   def test_point_to_svg
