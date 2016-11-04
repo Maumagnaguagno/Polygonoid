57a337dfd1c08d9578e55dfe1acf7d4f9ac68b58
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-11-04 19:36:59 -0200

    Upgrade grid style

--- NeonPolygon.rb
+++ NeonPolygon.rb
@@ -23,9 +23,9 @@ end
 def svg_grid(width, height, step = 10)
 "<defs>
 <pattern id=\"grid\" width=\"#{step}\" height=\"#{step}\" patternUnits=\"userSpaceOnUse\">
-  <path d=\"M 100 0 L 0 0 0 100\" fill=\"none\" stroke=\"gray\" stroke-width=\"1\"/>
+  <path d=\"M 100 0 L 0 0 0 100\" style=\"fill:none;stroke:gray;stroke-width:0.5\"/>
 </pattern>
 </defs>
 <rect fill=\"url(#grid)\" width=\"#{width}\" height=\"#{height}\"></rect>
-<circle r=\"2\"/>\n"
+<circle r=\"0.5\" style=\"fill:none;stroke:black;stroke-width:0.5\"/>\n"
 end
