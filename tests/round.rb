d72b4e5b7ccf47ca9ade394e975bbab4c8d661a3
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-11-04 21:28:23 -0200

    Add Circle tests

---
+++ tests/round.rb
@@ -0,0 +1,27 @@
+require 'test/unit'
+require './NeonPolygon'
+
+class Round < Test::Unit::TestCase
+
+  def test_circle_initialize
+    circle = Circle.new(1,2,3)
+    assert_equal(1, circle.cx)
+    assert_equal(2, circle.cy)
+    assert_equal(3, circle.radius)
+  end
+
+  def test_circle_perimeter
+    circle = Circle.new(1,2,3)
+    assert_equal(Math::PI * 6, circle.perimeter)
+  end
+
+  def test_circle_area
+    circle = Circle.new(1,2,3)
+    assert_equal(Math::PI * 3 ** 2, circle.area)
+  end
+
+  def test_circle_to_svg
+    circle = Circle.new(1,2,3)
+    assert_equal("<circle cx=\"1\" cy=\"2\" r=\"3\" style=\"fill:gray;stroke:black\"/>\n", circle.to_svg)
+  end
+end
