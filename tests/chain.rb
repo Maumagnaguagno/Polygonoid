a973dacd36666b1eb92b5e08106fced9128aa8f4
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-11-04 04:46:51 -0200

    Polyline tests

---
+++ tests/chain.rb
@@ -0,0 +1,30 @@
+require 'test/unit'
+require './NeonPolygon'
+
+class Chain  < Test::Unit::TestCase
+
+  def test_polyline_initialize
+    a = Point.new(23,89)
+    b = Point.new(155,0.8)
+    c = Point.new(400,0)
+    ab = Line.new(a,b)
+    bc = Line.new(b,c)
+    abc = Polyline.new(a, b, c)
+    assert_equal([a,b,c], abc.vertices)
+    assert_equal([ab, bc], abc.edges)
+  end
+
+  def test_polyline_perimeter
+    a = Point.new(23,89)
+    b = Point.new(155,0.8)
+    c = Point.new(400,0)
+    abc = Polyline.new(a, b, c)
+    perimeter = Math.hypot(23 - 155, 89 - 0.8) + Math.hypot(155 - 400, 0.8)
+    assert_equal(perimeter, abc.perimeter)
+  end
+
+  def test_polyline_to_svg
+    poly = Polyline.new(Point.new(1,2), Point.new(3,4), Point.new(5,6))
+    assert_equal("<polyline points=\"1,2 3,4 5,6 \" style=\"fill:none;stroke:black\"/>\n", poly.to_svg)
+  end
+end
