91b23baebc66ae6c06e6e22a1367329344a74084
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-11-26 19:13:41 -0200

    Make search a method

--- examples/Search.rb
+++ examples/Search.rb
@@ -41,6 +41,58 @@ def nearby(point)
   Point.new(x0, y0 - 5)
 end
 
+def search(start, goal, environment)
+  # SVG
+  svg = svg_grid(500, 500) << start.to_svg << goal.to_svg
+  environment.each {|polygon| svg << polygon.to_svg}
+
+  reachable_positions = [start]
+  visited = []
+  visible_points = []
+  index = 0
+  until reachable_positions.empty?
+    pos, plan = reachable_positions.shift
+    visited << pos
+    pos = nearby(pos)
+
+    index += 1
+    new_svg = svg.dup
+    puts "#{index}: Point (#{pos.x}, #{pos.y})"
+    # Goal visible test
+    if visible?(pos, goal, environment, new_svg)
+      puts '  Goal found'
+      # Build plan
+      final_plan = [pos, goal]
+      while plan
+        final_plan.unshift(plan.first)
+        plan = plan.last
+      end
+      final_plan.unshift(start)
+      # Draw path
+      new_svg << Polyline.new(*final_plan).to_svg('fill:none;stroke:green;stroke-width:0.5')
+      svg_save("search_t#{index}.svg", new_svg, 500, 500, 0, 0, 100, 100)
+      return final_plan
+    end
+
+    # Visible corners
+    plan = [pos, plan]
+    environment.each {|polygon|
+      polygon.vertices.each {|v|
+        if not visited.include?(v) and visible?(pos, v, environment, new_svg)
+          visible_points << [v, plan]
+          new_svg << Line.new(pos, v).to_svg('stroke:red;stroke-width:0.5')
+        end
+      }
+    }
+    svg_save("search_t#{index}.svg", new_svg, 500, 500, 0, 0, 100, 100)
+
+    # Visible points are reachable positions
+    reachable_positions.push(*visible_points).sort_by! {|p| p.first.distance(goal)}
+    reachable_positions.each {|p| puts "  Point (#{p.first.x}, #{p.first.y}) => Distance #{p.first.distance(goal)}"}
+    visible_points.clear
+  end
+end
+
 # Environment
 environment = [
   Polygon.new(
@@ -57,52 +109,4 @@ environment = [
 start = Point.new(80.0,50.0)
 goal = Point.new(15.0,50.0)
 
-# SVG
-svg = svg_grid(500, 500) << start.to_svg << goal.to_svg
-environment.each {|object| svg << object.to_svg}
-
-reachable_positions = [start]
-visited = []
-visible_points = []
-index = 0
-until reachable_positions.empty?
-  pos, plan = reachable_positions.shift
-  visited << pos
-  pos = nearby(pos)
-
-  index += 1
-  new_svg = svg.dup
-  puts "#{index}: Point (#{pos.x}, #{pos.y})"
-  # Goal visible test
-  if visible?(pos, goal, environment, new_svg)
-    puts 'Goal'
-    # Build plan
-    final_plan = [pos, goal]
-    while plan
-      final_plan.unshift(plan.first)
-      plan = plan.last
-    end
-    final_plan.unshift(start)
-    # Draw path
-    new_svg << Polyline.new(*final_plan).to_svg('fill:none;stroke:green;stroke-width:0.5')
-    svg_save("search_t#{index}.svg", new_svg, 500, 500, 0, 0, 100, 100)
-    break
-  end
-
-  # Visible corners
-  plan = [pos, plan]
-  environment.each {|polygon|
-    polygon.vertices.each {|v|
-      if not visited.include?(v) and visible?(pos, v, environment, new_svg)
-        visible_points << [v, plan]
-        new_svg << Line.new(pos, v).to_svg('stroke:red;stroke-width:0.5')
-      end
-    }
-  }
-  svg_save("search_t#{index}.svg", new_svg, 500, 500, 0, 0, 100, 100)
-
-  # Visible points are reachable positions
-  reachable_positions.push(*visible_points).sort_by! {|p| p.first.distance(goal)}
-  reachable_positions.each {|p| puts "  Point (#{p.first.x}, #{p.first.y}) => Distance #{p.first.distance(goal)}"}
-  visible_points.clear
-end
+search(start, goal, environment)
