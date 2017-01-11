52d8b8a0ce2ccb4ecf094f2b7632e894774837ba
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-01-11 16:18:12 -0200

    Move goalrtree problem to separate file

--- examples/goalrtree/GoalRtree.rb
+++ examples/goalrtree/GoalRtree.rb
@@ -1,4 +1,4 @@
-require_relative '../Polygonoid'
+require_relative '../../Polygonoid'
 
 def rect_to_polygon(x, y, w, h)
   Polygon.new(
@@ -139,34 +139,9 @@ def cluster_visible_rects(environment, goal_tree, max_distance, svg = nil, svg_f
   clusters
 end
 
-if $0 == __FILE__
+def find_tree(environment, goals)
   # Remove old files
   File.delete(*Dir.glob('partition*.svg'))
-
-  environment = [
-    # Rects [x,y,w,h]
-    [  0,   0, 500, 500],
-    [350,   0,  20, 150],
-    [245, 120,  20, 150],
-    [175, 270, 175,  20],
-    [245, 290,  20, 130],
-    [350, 270,  20, 230],
-    [370, 300, 130,  20],
-    [  0, 365, 130,  20]
-  ]
-
-  goals = [
-    Point.new(435,  75),
-    Point.new(480, 200),
-    Point.new(380, 250),
-    Point.new(300,  50),
-    Point.new(280, 200),
-    Point.new(200, 150),
-    Point.new(100, 150),
-    Point.new(230, 330),
-    Point.new( 50, 470),
-    Point.new(450, 470)
-  ]
 
   srand(2)
   svg = svg_grid(500, 500)
