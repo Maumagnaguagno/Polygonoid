8edb3bbed7c53f4e472a9eef79cddb8919ff329f
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-01-13 17:36:30 -0200

    Add Goal R-tree pb2 with bug to be solved

---
+++ examples/goalrtree/pb2.rb
@@ -0,0 +1,17 @@
+require_relative 'GoalRtree'
+
+environment = [
+  # Rects [x,y,w,h]
+  [0, 0, 100, 100],
+]
+
+goals = [
+  Point.new(10, 50),
+  Point.new(10, 60),
+  Point.new(90, 50),
+  Point.new(90, 60)
+]
+
+puts 'TODO: fix many goals within same rect'
+tree = find_goalrtree(environment, goals)
+puts tree
