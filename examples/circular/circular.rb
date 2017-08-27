e2ff3f6634835e086282ff2f269af36758782665
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-08-27 20:16:53 -0300

    Add more circles and visibility test to circular example

--- examples/circular/circular.rb
+++ examples/circular/circular.rb
@@ -26,6 +26,10 @@ def external_bitangent_lines(a, b, d)
   [Line.new(c, f), Line.new(d, e)]
 end
 
+def visible?(line, circles, a, b, svg = '')
+  circles.all? {|c| c == a or c == b or line.segment_distance_to_point(Point.new(c.cx, c.cy)) >= c.radius}
+end
+
 if $0 == __FILE__
   a = Circle.new(150, 150, 130)
   b = Circle.new(450, 150, 50)
@@ -46,7 +50,8 @@ if $0 == __FILE__
   # Forest
   svg = svg_grid(1100, 1100)
   srand(0)
-  circles = Array.new(5) {Circle.new(50 + rand(1000), 50 + rand(1000), 5 + rand(50))}
+  t = Time.now.to_f
+  circles = Array.new(10) {Circle.new(50 + rand(1000), 50 + rand(1000), 5 + rand(50))}
   circles.each {|c| svg << c.to_svg}
   i = 0
   circles.each {|a|
@@ -54,13 +59,16 @@ if $0 == __FILE__
       d = Math.hypot(a.cx - b.cx, a.cy - b.cy)
       if a.radius + b.radius <= d
         l1, l2 = internal_bitangent_lines(a, b, d)
-        svg << l1.to_svg('stroke:red') << l2.to_svg('stroke:red')
+        svg << l1.to_svg('stroke:red') if visible?(l1, circles, a, b, svg)
+        svg << l2.to_svg('stroke:red') if visible?(l2, circles, a, b, svg)
       end
       if (a.radius - b.radius).abs <= d
         l3, l4 = external_bitangent_lines(a, b, d)
-        svg << l3.to_svg('stroke:blue') << l4.to_svg('stroke:blue')
+        svg << l3.to_svg('stroke:blue') if visible?(l3, circles, a, b, svg)
+        svg << l4.to_svg('stroke:blue') if visible?(l4, circles, a, b, svg)
       end
     }
   }
+  p Time.now.to_f - t
   svg_save('bitangent_forest.svg', svg, 'viewbox="0 0 200 200"')
 end
