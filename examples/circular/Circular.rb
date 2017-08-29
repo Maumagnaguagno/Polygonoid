f4efafec5920e5de0897ee777dca9eabc92e2d62
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-08-29 03:23:06 -0300

    Rename Circular get_point to move

--- examples/circular/Circular.rb
+++ examples/circular/Circular.rb
@@ -1,28 +1,28 @@
 # Based on https://redblobgames.github.io/circular-obstacle-pathfinding/
 require_relative '../../Polygonoid'
 
-def get_point(cx, cy, radius, angle)
-  Point.new(cx + radius * Math.cos(angle), cy + radius * Math.sin(angle))
+def move(x, y, amount, angle)
+  Point.new(x + amount * Math.cos(angle), y + amount * Math.sin(angle))
 end
 
 def internal_bitangent_lines(a, b, d)
   angle = Math.acos((a.radius + b.radius).fdiv(d))
   ab = Math.atan2(b.cy - a.cy, b.cx - a.cx)
   ba = Math.atan2(a.cy - b.cy, a.cx - b.cx)
-  c = get_point(a.cx, a.cy, a.radius, ab - angle)
-  d = get_point(a.cx, a.cy, a.radius, ab + angle)
-  e = get_point(b.cx, b.cy, b.radius, ba + angle)
-  f = get_point(b.cx, b.cy, b.radius, ba - angle)
+  c = move(a.cx, a.cy, a.radius, ab - angle)
+  d = move(a.cx, a.cy, a.radius, ab + angle)
+  e = move(b.cx, b.cy, b.radius, ba + angle)
+  f = move(b.cx, b.cy, b.radius, ba - angle)
   [Line.new(c, f), Line.new(d, e)]
 end
 
 def external_bitangent_lines(a, b, d)
   angle = Math.acos((a.radius - b.radius).fdiv(d))
   ab = Math.atan2(b.cy - a.cy, b.cx - a.cx)
-  c = get_point(a.cx, a.cy, a.radius, ab - angle)
-  d = get_point(a.cx, a.cy, a.radius, ab + angle)
-  e = get_point(b.cx, b.cy, b.radius, ab + angle)
-  f = get_point(b.cx, b.cy, b.radius, ab - angle)
+  c = move(a.cx, a.cy, a.radius, ab - angle)
+  d = move(a.cx, a.cy, a.radius, ab + angle)
+  e = move(b.cx, b.cy, b.radius, ab + angle)
+  f = move(b.cx, b.cy, b.radius, ab - angle)
   [Line.new(c, f), Line.new(d, e)]
 end
 
