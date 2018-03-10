99bf448a502d6618e28aaacd618e7b6fb3cb8e71
Mau Magnaguagno <maumagnaguagno@gmail.com>
2018-03-10 06:08:51 -0300

    Improve Polygon center avoiding a call to area

--- src/Polygon.rb
+++ src/Polygon.rb
@@ -16,15 +16,15 @@ class Polygon < Polyline
   end
 
   def center
-    cx = cy = 0
+    a = cx = cy = 0
     p2 = @vertices.last
     @vertices.each {|p1|
-      n = (p1.x * p2.y - p2.x * p1.y)
+      a += n = (p1.x * p2.y - p2.x * p1.y)
       cx += (p1.x + p2.x) * n
       cy += (p1.y + p2.y) * n
       p2 = p1
     }
-    a6 = area * 6
+    a6 = a * 3
     Point.new(cx.fdiv(a6), cy.fdiv(a6))
   end
 
