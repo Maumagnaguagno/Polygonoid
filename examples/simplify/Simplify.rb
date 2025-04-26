8476ef8f21f59cd635286116f9e87d805fac5d36
Mau Magnaguagno <maumagnaguagno@gmail.com>
2025-04-26 00:02:08 -0300

    Add first and last parameters to simplify

--- examples/simplify/Simplify.rb
+++ examples/simplify/Simplify.rb
@@ -1,13 +1,13 @@
 require_relative '../../Polygonoid'
 
 # https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm
-def simplify(points, epsilon = 0.001)
-  first = points[0]
-  last = points[-1]
+def simplify(points, epsilon = 0.001, f = 0, l = points.size - 1)
+  first = points[f]
+  last = points[l]
   line = Line.new(first, last)
   # Find the point with the maximum distance
   max = index = 0
-  1.upto(points.size - 1) {|i|
+  (f + 1).upto(l - 1) {|i|
     d = line.segment_distance_to_point(points[i])
     if d > max
       index = i
@@ -16,8 +16,8 @@ def simplify(points, epsilon = 0.001)
   }
   # If max distance is greater than epsilon, recursively simplify
   if max > epsilon
-    (s1 = simplify(points[0..index], epsilon)).pop
-    s1.concat(simplify(points.drop(index), epsilon))
+    (s1 = simplify(points, epsilon, f, index)).pop
+    s1.concat(simplify(points, epsilon, index, l))
   else [first, last]
   end
 end
